bbb3cf7ab8db25c0ac05ecf65addc8e3dd967eeb
AmeerRagab <ameerragab1@yahoo.com>
2023-03-17 15:34:55 +0200

    Added events model for database

--- events/models.py
+++ events/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=80)
+    summary = models.TextField()
+    description = models.TextField()
+    url = models.URLField()
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True) # changes automatically as added
+    changed = models.DateTimeField(auto_now=True) # changes automatically as saved
+    status = models.TextField()
+    online_event = models.BooleanField()
+    hide_start_date = models.BooleanField()
+    hide_end_date = models.BooleanField()
+    

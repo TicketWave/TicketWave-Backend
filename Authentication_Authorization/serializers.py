e3e85794456adeb7d0dc591a1f9848d678dde2d8
ziad amr shalaby <zezoamr@yandex.com>
2023-03-21 20:32:01 +0200

    add my requirement file + pep8 formatting

--- Authentication_Authorization/serializers.py
+++ Authentication_Authorization/serializers.py
@@ -3,15 +3,18 @@ from dj_rest_auth.registration.serializers import RegisterSerializer
 
 
 class CustomRegisterSerializer(RegisterSerializer):
-    reg_no = serializers.CharField(max_length=10, write_only=True, required=True)
-    department = serializers.CharField(required=True, max_length=5, write_only=True)
-    university = serializers.CharField(required=True, max_length=100, write_only=True)
+    reg_no = serializers.CharField(
+        max_length=10, write_only=True, required=True)
+    department = serializers.CharField(
+        required=True, max_length=5, write_only=True)
+    university = serializers.CharField(
+        required=True, max_length=100, write_only=True)
 
     def get_cleaned_data(self):
         data_dict = super().get_cleaned_data()
-        
+
         data_dict['reg_no'] = self.validated_data.get('reg_no', '')
         data_dict['department'] = self.validated_data.get('department', '')
         data_dict['university'] = self.validated_data.get('university', '')
-        
+
         return data_dict
--- Authentication_Authorization/views.py
+++ Authentication_Authorization/views.py
@@ -1,14 +1,14 @@
 from rest_framework.response import Response
 
-from dj_rest_auth.registration.views import RegisterView 
+from dj_rest_auth.registration.views import RegisterView
 from .serializers import CustomRegisterSerializer
-
 
 
 class RegistrationView(RegisterView):
     serializer_class = CustomRegisterSerializer
-RegistrationView_func = RegistrationView.as_view()
 
+
+RegistrationView_func = RegistrationView.as_view()
 
 
 def empty_view():

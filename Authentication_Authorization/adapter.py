05ab5505675ef2f1648b4f9e11972bfc80130292
ziad amr shalaby <zezoamr@yandex.com>
2023-03-21 23:41:49 +0200

    Update adapter.py

--- Authentication_Authorization/adapter.py
+++ Authentication_Authorization/adapter.py
@@ -9,15 +9,12 @@ class CustomAccountAdapter(DefaultAccountAdapter):
     def save_user(self, request, user, form, commit=False):
         try:
             user = super().save_user(request, user, form, commit)
-            try:
-                data = form.cleaned_data
-                #user.is_public = data.get('is_public')
+            
+            data = form.cleaned_data
+            #user.is_public = data.get('is_public')
 
-                user.is_active = False
-                user.save()
-
-            except:
-                pass
+            user.is_active = False
+            user.save()
         except:
             pass
         return user

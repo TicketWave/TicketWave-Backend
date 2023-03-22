39f299605166088ad192ca2c329e5a3ccc68a475
AmeerRagab <ameerragab1@yahoo.com>
2023-03-22 20:06:15 +0200

    Events models andticket class api

---
+++ ticket_classes/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	path('', views.ApiOverview, name='home'),
+	path('create/', views.add_ticket_classes, name='add-ticket-classes'),
+	path('all/', views.view_ticket_classes, name='view-ticket-classes'),
+	path('update/<int:pk>/', views.update_tickets_classes, name='update-ticket-classes'),
+
+]

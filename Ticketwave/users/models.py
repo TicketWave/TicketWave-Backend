e9feb3e51a59c3aa21ddd1731d53c0ae7a0ce0d7
Abdelrahman Salem <117999599+ASalem22@users.noreply.github.com>
2023-03-24 00:26:22 +0200

    Update models.py

--- Ticketwave/users/models.py
+++ Ticketwave/users/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-class Users(AbstractUser):
+class users(AbstractUser):
   id=models.AutoField(primary_key=True)
   name=models.CharField(max_length=80)
   first_name=models.CharField(max_length=80)
   last_name=models.CharField(max_length=80)
   is_public=models.BooleanField(default=True)
   image_id=Models.CharField(max_length=200)
+  pass
+class users_emails(models.Model):
+  user_id=models.ForeignKey(users,to_field='id')
+  email=models.CharField(max_length=80)
+  verified=models.BooleanField(default=False)
   pass

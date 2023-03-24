75a8aba5463649ea9ece69728e62d325349686f1
Ahmed M. Gamal <amgamal200111@gmail.com>
2023-03-24 23:52:14 +0200

    fixed minor issues

--- Ticketwave/users/models.py
+++ Ticketwave/users/models.py
@@ -1,14 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from media.models import Media
-class users(AbstractUser):
-  id = models.AutoField(primary_key=True)
-  name = models.CharField(max_length=80)
-  first_name = models.CharField(max_length=80)
-  last_name = models.CharField(max_length=80)
+class Users(AbstractUser):
   is_public = models.BooleanField(default=True)
-  image_id = models.ForeignKey(to= Media, on_delete=models.PROTECT)
-class users_emails(models.Model):
-  user_id = models.ForeignKey(users, on_delete=models.CASCADE)
-  email = models.CharField(max_length=80)
-  verified = models.BooleanField(default=False)
+  image_id = models.IntegerField(default=-1)
--- Ticketwave/users/views.py
+++ Ticketwave/users/views.py
@@ -5,14 +5,14 @@ def home(request):
     return JsonResponse({'message': 'This is a test response.'})
 def user_profile(request):
     user=request.user
-    user_data={
-        'id'=user.id
-        'name'=user.name
-        'fist_name'=user.first_name
-        'last_name'=user.last_name
-        'is_public'=user.is_public
-        'image_id'=user.image_id
-        'email'=user_emails.email
-        'verified'=user_emails.verified
-    }
+    # user_data={
+    #     'id'=user.id
+    #     'name'=user.name
+    #     'fist_name'=user.first_name
+    #     'last_name'=user.last_name
+    #     'is_public'=user.is_public
+    #     'image_id'=user.image_id
+    #     'email'=user_emails.email
+    #     'verified'=user_emails.verified
+    # }
     return JsonResponse(user_data)

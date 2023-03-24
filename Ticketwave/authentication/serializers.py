5f582f5a9016b96f4d84fb2e521e921dd70bea7e
ziad amr shalaby <zezoamr@yandex.com>
2023-03-24 16:22:23 +0200

    user-auth-integration

--- Ticketwave/authentication/serializers.py
+++ Ticketwave/authentication/serializers.py
@@ -3,16 +3,15 @@ from dj_rest_auth.registration.serializers import RegisterSerializer
 
 
 class CustomRegisterSerializer(RegisterSerializer):
-    is_public = serializers.BooleanField(write_only=True, required=True)
+    is_public = serializers.BooleanField(write_only=True, required=True, default=True)
     image_id = serializers.IntegerField(
-        required=True, write_only=True)
-    
+        # required=True,
+        write_only=True, default=-1)
 
     def get_cleaned_data(self):
         data_dict = super().get_cleaned_data()
 
         data_dict['is_public'] = self.validated_data.get('is_public', '')
         data_dict['image_id'] = self.validated_data.get('image_id', '')
-
 
         return data_dict

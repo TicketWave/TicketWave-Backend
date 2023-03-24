3c2eb229592c1abe79c402ca757f7d73a9e8ca42
Ahmed M. Gamal <amgamal200111@gmail.com>
2023-03-24 16:23:11 +0200

    edit users

--- Ticketwave/users/models.py
+++ Ticketwave/users/models.py
@@ -3,14 +3,5 @@ from django.contrib.auth.models import AbstractUser
 from media.models import Media
 
 class Users(AbstractUser):
-  id = models.AutoField(primary_key=True)
-  name = models.CharField(max_length=80)
-  first_name = models.CharField(max_length=80)
-  last_name = models.CharField(max_length=80)
   is_public = models.BooleanField(default=True)
-  image_id = models.ForeignKey(to= Media, on_delete=models.PROTECT)
-
-class Users_emails(models.Model):
-  user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
-  email = models.CharField(max_length=80)
-  verified = models.BooleanField(default=False)
+  image_id = models.IntegerField()

88d191be4703a404317cd48273f0d7c2568c73d3
AmeerRagab <ameerragab1@yahoo.com>
2023-03-25 14:34:30 +0200

    fix position

--- Ticketwave/ticket_classes/urls.py
+++ Ticketwave/ticket_classes/urls.py
@@ -1,6 +1,10 @@
-from . import views
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', views.home)
+	path('', views.ApiOverview, name='home'),
+	path('create/', views.add_ticket_classes, name='add-ticket-classes'),
+	path('all/', views.view_ticket_classes, name='view-ticket-classes'),
+	path('update/<int:pk>/', views.update_tickets_classes, name='update-ticket-classes'),
+
 ]

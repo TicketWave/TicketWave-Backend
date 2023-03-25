d38e463344c1b727c641d07854f58bd9ba47b7fc
Abdelrahman Salem <117999599+ASalem22@users.noreply.github.com>
2023-03-25 19:32:21 +0200

    Update urls.py

--- Ticketwave/users/urls.py
+++ Ticketwave/users/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 from .views import UserById, UserProfile
 urlpatterns = [
-    path('users/<int:pk>/', UserById.as_view()),
+    path('users/<int:id>/', UserById.as_view()),
     path('user/', UserProfile.as_view()),
 ]

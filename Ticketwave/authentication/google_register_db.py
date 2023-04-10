269016937ca8f5589dd6bb78043d7de92abce743
ziad amr shalaby <zezoamr@yandex.com>
2023-04-11 00:01:12 +0200

    Update google_register_db.py

--- Ticketwave/authentication/google_register_db.py
+++ Ticketwave/authentication/google_register_db.py
@@ -4,7 +4,7 @@ import os
 
 
 client_id = '584138943946-lbr8f7p9rkf62eao96dorqb3v8p5q3io.apps.googleusercontent.com' #os.environ.get('GOOGLE_APP_ID')
-secret_key = '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' #os.environ.get('GOOGLE_SECRET_KEY')
+secret_key = 'the old leaked ones are deleted and new ones were made, don"T worry lol' #os.environ.get('GOOGLE_SECRET_KEY')
 
 # Get the current site
 current_site = Site.objects.get_current()

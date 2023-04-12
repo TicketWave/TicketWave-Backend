8fba306edb760e3948ec5f8beaf648c456a98641
ziad amr shalaby <zezoamr@yandex.com>
2023-04-13 01:07:07 +0200

    get user location functionality

--- Ticketwave/venues/views.py
+++ Ticketwave/venues/views.py
@@ -6,6 +6,7 @@ from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView, 
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from authentication.permissions import Is_venueowner
 from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly, IsAuthenticated, AllowAny
 from .geolocation import get_location_info
@@ -48,12 +49,16 @@ class venue_Create(CreateAPIView):
     #permission_classes = [IsAuthenticated] 
     
     def perform_create(self, serializer):
-        name = self.request.data.get('name')
-        latitude = self.request.data.get('latitude')
-        longitude = self.request.data.get('longitude')
+        name = self.request.data.get('name', None)
+        latitude = self.request.data.get('latitude', None)
+        longitude = self.request.data.get('longitude', None)
+        
+        name = self.request.data.get('name', None)
+        latitude = self.request.data.get('latitude', None)
+        longitude = self.request.data.get('longitude', None)
         
         # Check if name, latitude, and longitude are provided
-        if not name or not latitude or not longitude:
+        if name is None or not latitude is None or not longitude is None:
             return Response({'error': 'Name, latitude, and longitude are required'}, status=400)
 
         # Check if latitude and longitude are valid
@@ -63,22 +68,45 @@ class venue_Create(CreateAPIView):
         except ValueError:
             return Response({'error': 'Invalid latitude or longitude'}, status=400)
         
-        city, state, country = get_location_info(latitude, longitude)
-        # Clip city, state, and country if necessary
-        city = city[:32]
-        state = state[:32]
-        country = country[:32]
-        
-        address = f"{city}, {state}, {country}"
-        
-        # Update the serializer object with the new data
-        serializer.validated_data['name'] = name
-        serializer.validated_data['city'] = city
-        serializer.validated_data['state'] = state
-        serializer.validated_data['country'] = country
-        serializer.validated_data['address'] = address
+        if city is None or not state is None or not country is None:
+            city, state, country = get_location_info(latitude, longitude)
+            # Clip city, state, and country if necessary
+            city = city[:32]
+            state = state[:32]
+            country = country[:32]
+            
+            address = f"{city}, {state}, {country}"
+            
+            # Update the serializer object with the new data
+            serializer.validated_data['name'] = name
+            serializer.validated_data['city'] = city
+            serializer.validated_data['state'] = state
+            serializer.validated_data['country'] = country
+            serializer.validated_data['address'] = address
 
-        # Validate the serializer object
-        serializer.is_valid(raise_exception=True)
+            # Validate the serializer object
+            serializer.is_valid(raise_exception=True)
 
         serializer.save()
+        
+class venue_get_user_location(APIView):
+    def get(self, request, event_id):
+        try:
+            latitude = self.request.data.get('latitude', None)
+            longitude = self.request.data.get('longitude', None)
+            
+            if latitude is None or longitude is None: return Response(status=400)
+            
+            latitude = float(latitude)
+            longitude = float(longitude)
+            
+            city, state, country = get_location_info(latitude, longitude)
+            # Clip city, state, and country if necessary
+            city = city[:32]
+            state = state[:32]
+            country = country[:32]
+            
+            return Response({'city': city, 'state': state, 'country': country})
+        
+        except:
+            return Response(status=400)

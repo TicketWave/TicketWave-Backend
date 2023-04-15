284151ba5d917268b60c3aad0a75aaaab7107ea8
Abdelrahman Salem <117999599+ASalem22@users.noreply.github.com>
2023-04-15 08:55:35 +0200

    Update urls.py

--- Ticketwave/attendees/urls.py
+++ Ticketwave/attendees/urls.py
@@ -1,3 +1,11 @@
+from django.urls import path
+from . import views
+
 urlpatterns = [
-    
+    path('attendees/<int:attendee_id>/', get_attendee_by_id, name='get_attendee_by_id'),
+    path('attendees/event/<int:event_id>/', get_attendee_by_event, name='get_attendee_by_event'),
+    path('attendees/user/<int:user_id>/', get_attendee_by_user, name='get_attendee_by_user'),
+    path('attendees/', post_attendee, name='post_attendee'),
+    path('attendees/<int:attendee_id>/', put_attendee, name='put_attendee'),
+    path('attendees/<int:attendee_id>/', delete_attendee, name='delete_attendee'),   
 ]

ad4a1f27b507d2bebc39dedc906b7bd901411ae8
ziad amr shalaby <zezoamr@yandex.com>
2023-05-02 23:23:16 +0300

    Update serializers.py

--- Ticketwave/events/serializers.py
+++ Ticketwave/events/serializers.py
@@ -7,7 +7,8 @@ class event_Serializer(serializers.ModelSerializer):
         model = Event
         fields = ['id', 'name', 'summary', 'description', 'url', 'start', 'end', 'created', 'changed', 'status', 'online_event',
                 'hide_start_date', 'hide_end_date', 'free', 'waitlist', 'view_counter', 'owner', 'category',
-                'age_restriction', 'language', 'timezone', 'video_url', 'published', 'fully_booked', 'organizer']
+                'age_restriction', 'language', 'timezone', 'video_url', 'published', 'fully_booked', 'organizer', 'to_be_announced',
+                'recurring_end_day', "recurring_start_day", 'recurring']
         
         #exclude = ['listed', 'shareable', 'invite_only', 'show_remaining',
         #        'password', 'capacity', 'capacity_is_custom', 'followers']

57c22004ed4b9d5d0530c2ad7fb8dd2d74844168
ziad amr shalaby <zezoamr@yandex.com>
2023-05-03 00:37:42 +0300

    recurring more

--- Ticketwave/events/serializers.py
+++ Ticketwave/events/serializers.py
@@ -8,7 +8,7 @@ class event_Serializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'summary', 'description', 'url', 'start', 'end', 'created', 'changed', 'status', 'online_event',
                 'hide_start_date', 'hide_end_date', 'free', 'waitlist', 'view_counter', 'owner', 'category',
                 'age_restriction', 'language', 'timezone', 'video_url', 'published', 'fully_booked', 'organizer', 'to_be_announced',
-                'recurring_end_day', "recurring_start_day", 'recurring']
+                'recurring_end_day', "recurring_start_day", 'recurring_end_month', "recurring_start_month", 'recurring', 'recurring_frequency']
         
         #exclude = ['listed', 'shareable', 'invite_only', 'show_remaining',
         #        'password', 'capacity', 'capacity_is_custom', 'followers']

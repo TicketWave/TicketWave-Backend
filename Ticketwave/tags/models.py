320e463e314bc80098f2b0e0884d893b64621c67
ziad amr shalaby <zezoamr@yandex.com>
2023-05-02 22:57:21 +0300

    reverting last commit

--- Ticketwave/tags/models.py
+++ Ticketwave/tags/models.py
@@ -1,11 +1,9 @@
 from django.db import models
-from events.models import Event
 
 # Create your models here.
 
 class Tags(models.Model):
     name = models.CharField(max_length=80)
-    tags = models.ManyToManyField(Event, on_delete=models.CASCADE, related_name='tags')
     
     def __str__(self) -> str:
         return self.name

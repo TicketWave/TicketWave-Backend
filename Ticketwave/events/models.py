7efdaaef3e8ba21118786234b07f801bc0f5b28b
ziad amr shalaby <86983316+zezoamr@users.noreply.github.com>
2023-05-03 13:40:22 +0300

    Update models.py

--- Ticketwave/events/models.py
+++ Ticketwave/events/models.py
@@ -91,10 +91,10 @@ class Event(models.Model):
     category = models.ForeignKey(
         Categories, on_delete=models.SET_DEFAULT, default=1, related_name='event')
 
-    followers = models.ManyToManyField(Users, related_name='following_event', blank= True)
+    followers = models.ManyToManyField(Users, related_name='following_event', blank= True, null=True)
     venue = models.ForeignKey(
-        Venue, on_delete=models.CASCADE, related_name='event')
-    tags = models.ManyToManyField(Tags, on_delete=models.CASCADE, related_name='event', blank= True)
+        Venue, on_delete=models.CASCADE, related_name='event', null=True, blank=True)
+    tags = models.ManyToManyField(Tags, on_delete=models.CASCADE, related_name='event', blank= True, null=True)
 
     # private fields will limit access to by serializer
 

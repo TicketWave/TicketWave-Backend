5c717aff1f5a59f6afbf47f3e0334364dab4df0c
Abdelrahman Salem <117999599+ASalem22@users.noreply.github.com>
2023-05-06 21:52:51 +0300

    Update urls.py

--- Ticketwave/media/urls.py
+++ Ticketwave/media/urls.py
@@ -1,3 +1,8 @@
+from . import views
+from django.urls import path
+
 urlpatterns = [
-    
+    path('create/', views.createMedia.as_view()),
+    path('<int:pk>/', views.manageMedia.as_view()),
+    path('event/<int:event_id>/', views.listMediaByEvent)
 ]

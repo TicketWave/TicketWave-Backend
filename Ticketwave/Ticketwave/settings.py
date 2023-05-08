3c8642def56359c930ff86ee4e33518bf1dc4dbf
Ahmed M. Gamal <amgamal200111@gmail.com>
2023-05-09 00:43:32 +0300

    fix: migration issues

--- Ticketwave/Ticketwave/settings.py
+++ Ticketwave/Ticketwave/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY') #os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True #os.environ.get('DJANGO_DEBUG')
+DEBUG = False #os.environ.get('DJANGO_DEBUG')
 
 ALLOWED_HOSTS = ['backend', '127.0.0.1']
 

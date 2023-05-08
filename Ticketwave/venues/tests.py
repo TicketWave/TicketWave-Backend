f4f013c5a9a2f647994651bd6a3c1568d4acbb20
ziad amr shalaby <zezoamr@yandex.com>
2023-05-08 20:45:26 +0300

    venue unit tests

--- Ticketwave/venues/tests.py
+++ Ticketwave/venues/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from geopy.geocoders import Nominatim
 
 # Create your tests here.
+
+class VenueTestCase(TestCase):
+    def test_IncrementView_Serizalizer(self):
+        geolocator = Nominatim(user_agent="ticketwave")
+        location = geolocator.reverse(f'48.858093, 2.294694')
+        address = location.raw['address']
+        city = address.get('city', '')
+        state = address.get('state', '')
+        country = address.get('country', '')
+        self.assertEqual(state, 'Île-de-France')
+        self.assertEqual(city,'Paris' )
+        self.assertEqual(country, 'France')

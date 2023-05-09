1f0d3ea599ebfb751d7a25eb40d1384f38253852
Ahmed M. Gamal <amgamal200111@gmail.com>
2023-05-09 14:15:18 +0300

    funvtion documentations

--- Ticketwave/discounts/views.py
+++ Ticketwave/discounts/views.py
@@ -5,25 +5,30 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 import datetime
 
+
 class manageDiscounts(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = DiscountsSerializer
     queryset = Discounts.objects.all()
-    lookup_field = 'id'
+    lookup_field = "id"
+
 
 class createDiscount(generics.CreateAPIView):
     serializer_class = DiscountsSerializer
     queryset = Discounts.objects.all()
 
-@api_view(('GET',))
+
+@api_view(("GET",))
 def listDiscountsByEvent(request, event_id):
     discounts = Discounts.objects.filter(event_id=event_id)
-    serializer = DiscountsSerializer(discounts, many = True)
+    serializer = DiscountsSerializer(discounts, many=True)
     return Response(serializer.data)
 
+
 def applyDiscount(discount_code):
-    discount = Discounts.objects.get(code = discount_code)
+    discount = Discounts.objects.get(code=discount_code)
     if discount.quantity_available > 0 and discount.end_date >= datetime.datetime.now():
         discount.quantity_sold += 1
         discount.quantity_available -= 1
         discount.save()
-    else: return None
+    else:
+        return None

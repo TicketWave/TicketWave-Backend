7fcda11f1c8eac6bf28b3ce5756e77f579054d06
Ahmed M. Gamal <amgamal200111@gmail.com>
2023-05-09 20:07:28 +0300

    more changes

--- Ticketwave/discounts/urls.py
+++ Ticketwave/discounts/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path("create/", views.createDiscount.as_view()),
-    path("<int:pk>/", views.manageDiscounts.as_view()),
+    path("<str:discount_code>/", views.manageDiscounts.as_view()),
     path("event/<int:event_id>/", views.listDiscountsByEvent),
     path("apply/<str:discount_code>", views.applyDiscount),
-    path("check/<str:discount_code>", views.applyDiscount),
+    path("check/<str:discount_code>", views.checkDiscount),
 ]

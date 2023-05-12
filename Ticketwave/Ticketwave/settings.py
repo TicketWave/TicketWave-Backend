9b84954509dddf7164eecdc7ebb7dc670fba9d8d
Omar Atef <85079143+Yalab7@users.noreply.github.com>
2023-05-12 21:28:16 +0300

    Fixed ALLOWED_HOSTS

--- Ticketwave/Ticketwave/settings.py
+++ Ticketwave/Ticketwave/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")  # os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True  # os.environ.get('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['backend']
 
 
 # Application definition

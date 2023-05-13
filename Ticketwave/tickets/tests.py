ac187914fbb86064a2248bc1bd1cb806915ba853
AmeerRagab <43808821+AmeerRagab@users.noreply.github.com>
2023-05-13 22:45:27 +0300

    Fixed unit tests

--- Ticketwave/tickets/tests.py
+++ Ticketwave/tickets/tests.py
@@ -1,59 +1,29 @@
 from django.test import TestCase
 from .models import Ticket
-import json
-import requests
-
+from .serlializers import TicketSerializer
 
 # Create your tests here.
 
 
 class TicketTestCase(TestCase):
     def setUp(self):
-        Ticket.objects.create(
-            name="Amir", category="Testing", subcategory="subtest", amount=10
-        )
-        Ticket.objects.create(
-            name="youssef", category="Testing", subcategory="subtest", amount=1000
-        )
+        Ticket.objects.create(name="Amir", category="Testing", subcategory="subtest", amount=10)
+        Ticket.objects.create(name="youssef", category="Testing", subcategory="subtest", amount=1000)
+        AmirSetup=Ticket.objects.create(name="Amir", category="Testing", subcategory="subtest", amount=10)
+        YoussefSetup=Ticket.objects.create(name="youssef", category="Testing", subcategory="subtest", amount=1000)
+        self.AmirSerializer=TicketSerializer(AmirSetup)
+        self.YoussefSerializer=TicketSerializer(YoussefSetup)
         # record created in database
 
     def test_Ticket_Model(self):
         Amir = Ticket.objects.get(name="Amir")
         youssef = Ticket.objects.get(name="youssef")
-        self.assertEqual(Amir.name, "Amir")
-        self.assertEqual(youssef.name, "youssef")
+        self.assertEqual(Amir.category, "Testing")
+        self.assertEqual(youssef.category, "Testing")
         # test database record is created successfully
 
-    def test_TicketEndpointCreate(self):  # testing create
-        url = "127.0.0.1:8000/tickets/create"
-        data = json.dumps(
-            {
-                "name": "Abdallah",
-                "category": "endpoint testing",
-                "subcategory": "subcategory is testing",
-                "amount": 100,
-            }
-        )
-        headers = {
-            "Content-Type": "application/json"  # specify content type as json object
-        }
-        response = requests.post(url=url, data=data, headers=headers)
-        self.assertEqual(response.status_code, 201)
-
-    def test_TicketEndpointGetAll(self):  # testing get all
-        url = "127.0.0.1:8000/tickets/get"
-        response = requests.get(url=url)
-        self.assertEqual(response.status_code, 200)
-
-    def test_TicketEndpointUpdate(self):  # testing update
-        url = "127.0.0.1:8000/tickets/update/2"
-        data = json.dumps(
-            {
-                "amount": "500",
-            }
-        )
-        headers = {
-            "Content-Type": "application/json"  # specify content type as json object
-        }
-        response = requests.put(url=url, data=data, headers=headers)
-        self.assertEqual(response.status_code, 200)
+    def TestTicketSerializer(self):
+        AmirData=self.AmirSerializer.data
+        YoussefData=self.YoussefSerializer.data
+        self.assertEqual(AmirData["name"], "Amir")
+        self.assertEqual(YoussefData["name"], "youssef")

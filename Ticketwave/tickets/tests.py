7d7fbe3f8e3a491be8b387e0878cf5b4d5f669d3
Ahmed M. Gamal <amgamal200111@gmail.com>
2023-05-13 18:07:33 +0300

    fixing tests

--- Ticketwave/tickets/tests.py
+++ Ticketwave/tickets/tests.py
@@ -4,49 +4,56 @@ import json
 import requests
 
 
-
 # Create your tests here.
+
 
 class TicketTestCase(TestCase):
     def setUp(self):
-        Ticket.objects.create(name="Amir", category="Testing", subcategory="subtest", amount=10)
-        Ticket.objects.create(name="youssef", category="Testing", subcategory="subtest", amount=1000)
-        #record created in database
-        
-    def Test_Ticket_Model(self):
-        Amir= Ticket.objects.get(name="Amir")
-        youssef= Ticket.objects.get(name="youssef")
+        Ticket.objects.create(
+            name="Amir", category="Testing", subcategory="subtest", amount=10
+        )
+        Ticket.objects.create(
+            name="youssef", category="Testing", subcategory="subtest", amount=1000
+        )
+        # record created in database
+
+    def test_Ticket_Model(self):
+        Amir = Ticket.objects.get(name="Amir")
+        youssef = Ticket.objects.get(name="youssef")
         self.assertEqual(Amir.name, "Amir")
         self.assertEqual(youssef.name, "youssef")
         # test database record is created successfully
-    
-    def TestTicketEndpointCreate(self):  #testing create
-        url="127.0.0.1:8000/tickets/create"
-        data= json.dumps({
-            "name":"Abdallah", 
-            "category": "endpoint testing",
-            "subcategory": "subcategory is testing",
-            "amount": 100
-        })
-        headers= {
-            'Content-Type':'application/json'    #specify content type as json object
+
+    def test_TicketEndpointCreate(self):  # testing create
+        url = "127.0.0.1:8000/tickets/create"
+        data = json.dumps(
+            {
+                "name": "Abdallah",
+                "category": "endpoint testing",
+                "subcategory": "subcategory is testing",
+                "amount": 100,
+            }
+        )
+        headers = {
+            "Content-Type": "application/json"  # specify content type as json object
         }
-        response= requests.post(url=url,data=data,headers=headers)
+        response = requests.post(url=url, data=data, headers=headers)
         self.assertEqual(response.status_code, 201)
 
+    def test_TicketEndpointGetAll(self):  # testing get all
+        url = "127.0.0.1:8000/tickets/get"
+        response = requests.get(url=url)
+        self.assertEqual(response.status_code, 200)
 
-    def TestTicketEndpointGetAll(self):  #testing get all
-        url="127.0.0.1:8000/tickets/get"
-        response= requests.get(url=url)
-        self.assertEqual(response.status_code, 200) 
-
-    def TestTicketEndpointUpdate(self):  #testing update
-        url="127.0.0.1:8000/tickets/update/2"
-        data= json.dumps({
-            "amount":"500", 
-        })
-        headers= {
-            'Content-Type':'application/json'    #specify content type as json object
+    def test_TicketEndpointUpdate(self):  # testing update
+        url = "127.0.0.1:8000/tickets/update/2"
+        data = json.dumps(
+            {
+                "amount": "500",
+            }
+        )
+        headers = {
+            "Content-Type": "application/json"  # specify content type as json object
         }
-        response= requests.put(url=url,data=data,headers=headers)
-        self.assertEqual(response.status_code, 200) 
+        response = requests.put(url=url, data=data, headers=headers)
+        self.assertEqual(response.status_code, 200)
